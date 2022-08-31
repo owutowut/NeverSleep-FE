3ce504d3b6b41c6ac79a3a9817edbdbad85472ac
Sarawut Bunmee <owutnr@gmail.com>
2022-09-01 06:05:37 +0700

    fix '>' to &gt;

--- components/NavRoute.js
+++ components/NavRoute.js
@@ -4,7 +4,7 @@ const NavRoute = () => {
     return (
         <div className='flex space-x-3 text-blue-500'>
             <label className='cursor-pointer'>Project management</label>
-            <label> > </label>
+            <label> &gt; </label>
             <label className='cursor-pointer'>Invoice</label>
         </div>
     );
