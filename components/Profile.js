89470ab5e3723fd9733c96c330905b06758c8389
Sarawut Bunmee <owutnr@gmail.com>
2022-09-01 06:45:54 +0700

    img to Image react

--- components/Profile.js
+++ components/Profile.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Image from 'next/image'
 
 const Profile = () => {
     return (
         <div className='flex p-4 space-x-4 bg-gray-100 rounded-xl shadow lg:w-2/5 lg:self-center'>
             <div className='self-center'>
-                <img className='h-10 w-10' src="/neverSleepIcon.png" alt='Profile'/>
+                <Image width={50} height={50} src="/neverSleepIcon.png" alt='Profile'/>
             </div>
             <div className='self-center'>
                 <p className='text-gray-400'>Accounting Manager</p>
